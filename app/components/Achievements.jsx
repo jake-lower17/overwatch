91e1e97f1258d8f77016f71b566646f39c95404f
Jacob Lower <jake.lower17@gmail.com>
2017-02-14 10:57:26 -0600

    Achievement styles

--- app/components/Achievements.jsx
+++ app/components/Achievements.jsx
@@ -9,11 +9,17 @@ export class Achievement extends React.Component {
     dispatch(actions.startAddAchievements('snake187eh'));
   }
 
-
+  decodeHtml (html) {
+    var txt = document.createElement('textarea');
+    txt.innerHTML = html;
+    return txt.value;
+  }
 
   render () {
+
     var { achievements } = this.props;
     var renderAchievements = () => {
+      var classNameAchievement = 'achievement-container';
       if (achievements.length === 0) {
         return (
           <p className="container__message">Loading.....</p>
@@ -21,14 +27,26 @@ export class Achievement extends React.Component {
       }
 
       return achievements.map((achievement) => {
+
+        if (achievement.finished) {
+          classNameAchievement = `${classNameAchievement} finished-achievement`;
+        }else {
+          classNameAchievement = 'achievement-container';
+        }
+        var titleNew = this.decodeHtml(achievement.name);
+        var descriptionNew = this.decodeHtml(achievement.description);
         return (
-          <div className="data-cell notes">{achievement.name}</div>
+          <div className={classNameAchievement}>
+            <h3>{titleNew}</h3>
+            <p>{descriptionNew}</p>
+            <img src={achievement.image} />
+          </div>
         );
       });
     };
 
     return (
-      <div>
+      <div className="achievements">
         {renderAchievements()}
       </div>
     );
